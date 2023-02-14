a148df371ae6b6b76946b24dea53cf89587dcdfa
gildemardiniz <gildemardiniz@gmail.com>
2023-02-13 23:09:17 -0300

    mudar cor do appbar pra cor official

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Green Berets',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: corGreenBerets,
       ),
       home: const MainPage(),
     );
@@ -30,77 +28,77 @@ class MyApp extends StatelessWidget {
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
-
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
-
   int _indiceAtual = 0;
 
   final List<Widget> _telas = [
-   MyHomePage(),
-   Eventos(),
-   Recrutamento(),
-   MinhaConta(),
+    MyHomePage(),
+    Eventos(),
+    Recrutamento(),
+    MinhaConta(),
   ];
 
-  final List<String> _titles =[
+  final List<String> _titles = [
     "Green Berets Airsoft",
     "Eventos",
     "Recrutamento",
     "Minha Conta",
   ];
 
-
-
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
-      appBar:
-          AppBar(
-          //centerTitle: true,
-          title: Text(_titles[_indiceAtual].toString()),
-        ),
-
+      appBar: AppBar(
+        //centerTitle: true,
+        title: Text(_titles[_indiceAtual].toString()),
+      ),
       body: _telas[_indiceAtual],
-
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: _indiceAtual,
         onTap: onTabTapped,
         items: const [
-
+          BottomNavigationBarItem(icon: Icon(Icons.house), label: "Início"),
           BottomNavigationBarItem(
-              icon: Icon(Icons.house),
-              label: "Início"
+            icon: Icon(Icons.calendar_month),
+            label: "Eventos",
           ),
-
           BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_month),
-              label:"Eventos",
-          ),
-
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_search),
-              label: "Recrutamento",
+            icon: Icon(Icons.person_search),
+            label: "Recrutamento",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
             label: "Minha Conta",
           ),
-
         ],
       ),
-
     );
   }
+
   void onTabTapped(int index) {
     setState(() {
       _indiceAtual = index;
     });
   }
 }
+
+const MaterialColor corGreenBerets = const MaterialColor(
+  0xff004B1D,
+  const <int, Color>{
+    50: Color(0xff004B1D),
+    100: Color(0xff004B1D),
+    200: Color(0xff004B1D),
+    300: Color(0xff004B1D),
+    400: Color(0xff004B1D),
+    500: Color(0xff004B1D),
+    600: Color(0xff004B1D),
+    700: Color(0xff004B1D),
+    800: Color(0xff004B1D),
+    900: Color(0xff004B1D),
+  },
+);
